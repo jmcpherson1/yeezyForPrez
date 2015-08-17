7b2dd7cd7ae7571de0724716e90262ff15e21fef
Jamasa McPherson <jamasamcpherson@Jamasas-MBP.home>
2015-08-16 22:19:38 -0400

    Before jQuery background slide implement.

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
 	$('#joinButton').click(function() {
-		$('#joinButton').alert("HI!");
+		alert( "You made the right decision. Yezzus will save us all!" );
 	});
+});
+
+$(".box").bgswitcher({
+  images: ["pic1.jpg", "pic2.jpg", "pic3.jpg"], // Background images
+  effect: "fade" // fade, blind, clip, slide, drop, hide
+});
+
+$(".box").bgswitcher({
+  images: ["pic1.jpg", "pic2.jpg", "pic3.jpg"], // Background images
+  effect: "fade", // fade, blind, clip, slide, drop, hide
+  interval: 5000, // Interval of switching
+  loop: true, // Loop the switching
+  shuffle: false, // Shuffle the order of an images
+  duration: 5000, // Effect duration
+  easing: "swing" // Effect easing
 });
