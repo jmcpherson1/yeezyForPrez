f7caf8ed48f2e11d1a212c581659292a341cde60
Jamasa McPherson <jamasamcpherson@Jamasas-MBP.home>
2015-08-16 23:33:29 -0400

    Added logo. Changed nav hover color.

--- js/custom.js
+++ js/custom.js
@@ -4,17 +4,3 @@ $(document).ready(function(){
 	});
 });
 
-$(".box").bgswitcher({
-  images: ["pic1.jpg", "pic2.jpg", "pic3.jpg"], // Background images
-  effect: "fade" // fade, blind, clip, slide, drop, hide
-});
-
-$(".box").bgswitcher({
-  images: ["pic1.jpg", "pic2.jpg", "pic3.jpg"], // Background images
-  effect: "fade", // fade, blind, clip, slide, drop, hide
-  interval: 5000, // Interval of switching
-  loop: true, // Loop the switching
-  shuffle: false, // Shuffle the order of an images
-  duration: 5000, // Effect duration
-  easing: "swing" // Effect easing
-});
